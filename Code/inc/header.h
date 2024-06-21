e63df576457f7d0a6989e2d062090d0bc7c819f4
yd8421 <yd8421@gmail.com>
2024-06-21 10:42:27 +0000

    Created a Code Repo for sprint

---
+++ Code/inc/header.h
@@ -0,0 +1,16 @@
+#ifndef __HEADER__
+#define __HEADER__
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node{
+}User;
+
+extern void myflush();
+extern void main_menu();
+extern void register_user();
+extern void login();
+extern void call();
+
+#endif
---
+++ Code/src/c_funct.c
@@ -0,0 +1,60 @@
+#include<header.h>
+
+void register_user(void);
+
+void myflush(void){
+	while(getchar() != '\n');
+}
+
+void main_menu(void){
+	
+	int try = 4;
+	while(try--){
+
+		system("clear");
+
+		int choice;
+
+		printf("******************************************\n");
+		printf("          Welcome to Main Menu            \n");
+		printf("******************************************\n\n");
+        	printf("\t1. Make a phone call\n");
+        	printf("\t2. Register for CFS\n");
+        	printf("\t3. Login to CFS\n");
+        	printf("\t4. Exit\n");
+		printf("\n******************************************\n");
+		
+		if(try<3){
+			printf("\nWrong input. You have %d tries left. Try Again", try+1);
+		}
+		printf("\nEnter your choice (1-4): ");
+
+		scanf("%d", &choice);
+		myflush();
+
+		switch(choice){
+
+			case 1: call();
+				break;
+
+			case 2: register_user();
+				break;
+
+			case 3: login();
+				break;
+
+			case 4: 
+				break;
+
+			default:
+				continue;
+		}
+		break;
+	}
+	printf("\nExiting the program\n\n");
+}
+
+void login(void){}
+void register_user(void){}
+void call(void){}
+
