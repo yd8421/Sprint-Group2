a118011775118442159ae795f7d14631de39cfa0
Afthabs299 <afthabs299@gmail.com>
2024-06-24 10:08:25 +0000

    Update option implemented

--- Code/src/admin_funct.c
+++ Code/src/admin_funct.c
@@ -4,6 +4,9 @@
 
 int login(void);
 int admin_menu(void);
+int update_menu(void);
+void create_command(char[], int);
+
 void add(void);
 void delete(void);
 void update(void);
@@ -35,6 +38,96 @@ int login()
     else
         return -1;
 }
+
+void create_command(char command[], int ch){
+	char client_number[11];
+	char forwarding_number[11];
+	int registered=0;
+	int forward_activated=0;
+	char forwarding_type;
+	int i=0;
+        
+	printf("\nEnter client number: ");
+        scanf("%s", client_number);
+        myflush();
+        
+	if(ch == 0 || ch == 1){
+		printf("\nEnter forwarding number: ");
+        	scanf(" %s", forwarding_number);
+	}
+	else{
+		strcpy(forwarding_number, "NA");
+	}
+
+        myflush();
+        if(ch == 0){
+		printf("\nRegister/Unregister user(0/1): ");
+        	scanf("%d", &registered);
+	}
+	else if(ch == 4){
+		registered = 0;
+	}
+	else{
+		registered = -1;
+	}
+
+        if(ch == 0 || ch == 3){
+		printf("\nActivate Service(0/1): ");
+        	scanf("%d", &forward_activated);
+        	myflush();
+	}
+	else{
+		forward_activated = -1;
+	}
+
+        if(ch == 2 || ch == 0){
+		printf("\nforward type\nUnconditional - U\nNo reply - N\nBusy - B\n(U/N/B): ");
+        	scanf("%c", &forwarding_type);
+       		myflush();
+	}
+	else{
+		forwarding_type = 'O';
+	}
+
+        if(ch == 0){
+		strcpy(command, "ADD_USER");
+        	i += 8;
+	}
+	else{
+		strcpy(command, "UPD_USER");
+		i += 8;
+	}
+
+        command[i++] = ' ';
+
+        for(int j=0; j<10; j++){
+
+                command[i++] = client_number[j];
+        }
+        command[i++] = ' ';
+
+        for(int j=0; j<strlen(forwarding_number); j++){
+                command[i++] = forwarding_number[j];
+        }
+        command[i++] = ' ';
+
+	if(registered != -1)command[i++] = '0' + registered;
+	else{ 
+		command[i++] = '-';
+		command[i++] = '1';
+	}
+	command[i++] = ' ';
+
+	if(forward_activated != -1)command[i++] = '0' + forward_activated;
+	else{ 
+		command[i++] = '-';
+		command[i++] = '1';
+	}
+        command[i++] = ' ';
+
+        command[i++] = forwarding_type;
+}
+
 
 int admin_menu(void){
 	
@@ -71,61 +164,57 @@ int admin_menu(void){
 
 }
 
+int update_menu(){
+
+        int try = 3;
+        int choice;
+
+        while(try--){
+
+                system("clear");
+
+                printf("******************************************\n");
+                printf("               Update Menu                \n");
+                printf("******************************************\n\n");
+                printf("\t1. Forwarding number\n");
+                printf("\t2. Forwarding type\n");
+                printf("\t3. Enable/Diasble forwarding\n");
+                printf("\t4. Unregister for CFS\n");
+                printf("\t5. Back to main menu\n");
+                printf("\n******************************************\n");
+
+                if(try<3){
+                        printf("\nWrong input. You have %d tries left. Try Again", try+1);
+                }
+                printf("\nEnter your choice (1-5): ");
+
+                scanf("%d", &choice);
+                myflush();
+
+                if(choice < 1 || choice > 5)continue;
+                else break;
+        }
+
+        return choice;
+}
+
 void fill_command(char command[],int choice)
 {
-	char client_number[11];
-	char forwarding_number[11];
-	int registered=0;
-	int forward_activated=0;
-	char forwarding_type;
-	int i=0;
 
+	int ch;
 	switch(choice)
 	{
 		case 1: strcpy(command,"VIEW_USER");
 		        break;
 
-		case 2: printf("\nEnter client number: ");
-			scanf("%s", client_number);
-			myflush();
-			printf("Enter forwarding number: ");
-			scanf(" %s", forwarding_number);
-			myflush();
-			printf("Register/Unregister user(0/1): ");
-			scanf("%d", &registered);
-			printf("Activate Service(0/1): ");
-			scanf("%d", &forward_activated);
-			myflush();
-			printf("forward type\nUnconditional - U\nNo reply - N\n Busy - B\n(U/N/B): ");
-			scanf("%c", &forwarding_type);
-			myflush();
-			strcpy(command, "ADD_USER");
-			i += 8;
-			command[i++] = ' ';
-			
-			for(int j=0; j<10; j++){
-				command[i++] = client_number[j];
-			}
-			command[i++] = ' ';
-
-			for(int j=0; j<10; j++){
-				command[i++] = forwarding_number[j];
-			}
-			command[i++] = ' ';
-
-			command[i++] = '0' + registered;
-			command[i++] = ' ';
-			
-			command[i++] = '0' + forward_activated;
-			command[i++] = ' ';
-
-			command[i++] = forwarding_type;
-
+		case 2: create_command(command, 0);
 			break;
 
-		case 3:
-
+		case 3: 
+			ch = update_menu();
+			create_command(command, ch);
 			break;
+
 		case 4: 
 			break;
 	}
